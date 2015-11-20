15c07fb54587a606a6fd0b68cefdb145e98c7cb2
Luzer59 <MikkoJuu@AOTA1-1L160-03D.kamit.fi>
2015-11-20 20:18:14 +0200

    Mouse following and backround

---
+++ Assets/Scripts/AimTarget.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AimTarget : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerAim.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAim : MonoBehaviour
+{
+    private Transform aimTarget;
+
+    void Awake()
+    {
+        aimTarget = GameObject.Find("AimTarget").transform;
+    }
+
+    void Update()
+    {
+        /*Quaternion newRotation = Quaternion.LookRotation(aimTarget.position);
+        newRotation *= Quaternion.Euler(0f,-90f,0f);  
+        GetComponent<Rigidbody2D>().rotation = newRotation;*/
+        if (aimTarget.position.y >= transform.position.y)
+        {
+            GetComponent<Rigidbody2D>().rotation = Vector3.Angle(Vector3.right, aimTarget.position - transform.position);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().rotation = 360f - Vector3.Angle(Vector3.right, aimTarget.position - transform.position);
+        }
+
+        Debug.DrawRay(transform.position, transform.right * 5f);
+    }
+}
