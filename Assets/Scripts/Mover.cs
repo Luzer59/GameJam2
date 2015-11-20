ff2329d3e19bc82b824ae9266afd3465f89f4f52
LeeviPaa <LeeviPaa@AOTA1-1L160-10D.kamit.fi>
2015-11-21 01:07:40 +0200

    Lisätty vihollis animaatiot

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,21 +4,26 @@ using System.Collections;
 public class Mover : MonoBehaviour {
 
     public float speed;
+    public bool StartAttacking = false;
 
 	void Start () {
+        animator = GetComponent<Animator>();
         if (GetComponent<Rigidbody>().position.x < 0)
         {
             GetComponent<Rigidbody>().velocity = transform.right * speed;
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-0.5f, 0.5f, 1);
         }
         if (GetComponent<Rigidbody>().position.x >= 0)
         {
             GetComponent<Rigidbody>().velocity = transform.right * -speed;
         }
 	}
+    Animator animator;
     void OnTriggerEnter(Collider StopPosition)
     {
         GetComponent<Rigidbody>().velocity = transform.right * 0;
+        StartAttacking = true;
+        bool asd = true;
+        animator.SetBool("Attacking", asd);
     }
-	
 }
