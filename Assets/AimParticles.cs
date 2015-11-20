fa29e3b8063e95412f37ccd9a4e66158bd9ca1fe
Luzer59 <MikkoJuu@AOTA1-1L160-03D.kamit.fi>
2015-11-20 23:49:12 +0200

    Fire particle system and 3d collider for ground

---
+++ Assets/AimParticles.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AimParticles : MonoBehaviour
+{
+    private ParticleCollisionEvent[] collisionEvents;
+    private ParticleSystem part;
+
+    void Awake()
+    {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new ParticleCollisionEvent[16];
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int safeLength = part.GetSafeCollisionEventSize();
+        if (collisionEvents.Length < safeLength)
+            collisionEvents = new ParticleCollisionEvent[safeLength];
+
+        int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+
+        for (int i = 0; i < numCollisionEvents; i++)
+        {
+            Debug.Log(collisionEvents[i].intersection);
+            /*Vector3 pos = collisionEvents[i].intersection;
+            Vector3 force = collisionEvents[i].velocity * 10;*/
+        }
+    }
+}
