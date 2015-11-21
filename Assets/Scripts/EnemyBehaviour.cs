aecf9edbf1b5bc13c2331bd32a3db21a93084bd2
LeeviPaa <LeeviPaa@AOTA1-1L160-10D.kamit.fi>
2015-11-21 13:29:02 +0200

    korjattu enemyDeath script

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -7,16 +7,22 @@ public class EnemyBehaviour : MonoBehaviour {
     //public GameObject Player;
     public int EnemyHealth = 100;
 
+    void Update()
+    {
+        if (EnemyHealth <= 0)
+        {
+            EnemyDeath();
+        }
+    }
+
     public void TakeDamage(int damage)
     {
         EnemyHealth -= damage;
     }
+
     void EnemyDeath()
     {
-        if (EnemyHealth <= 0)
-        {
             Destroy(gameObject);
-        }
     }
 
 }
