e16317f5e6fc4d3deb89def80758bc8b5f15c76c
LeeviPaa <LeeviPaa@AOTA1-1L160-10D.kamit.fi>
2015-11-21 15:21:06 +0200

    Lisätty vihollisille palamisefekti

---
+++ Assets/Scripts/BurningAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BurningAnimation : MonoBehaviour 
+{
+
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+public void changesprite(bool onfire)
+    {
+        animator.SetBool("Burning", onfire);
+    }
+}
--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -6,12 +6,35 @@ public class EnemyBehaviour : MonoBehaviour {
     //public GameObject Tower;
     //public GameObject Player;
     public int EnemyHealth = 100;
+    int maxhealth = 100;
+    bool isdead = false;
+    bool onfire = false;
+    private int BurningDamage = 10;
+    Animator animator;
+    public GameObject Fire;
+    
+    void Start()
+    {
+        GameObject Fire = new GameObject();
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
+        if(EnemyHealth <= 0.8f*maxhealth)
+        {
+
+            onfire = true;
+            Fire.GetComponent<BurningAnimation>().changesprite(onfire);
+            StartCoroutine(Burning());
+
+        }
+        
         if (EnemyHealth <= 0)
         {
-            EnemyDeath();
+            isdead = true;
+            animator.SetBool("Dead", isdead);
+            StartCoroutine(EnemyDeath());
         }
     }
 
@@ -20,9 +43,22 @@ public class EnemyBehaviour : MonoBehaviour {
         EnemyHealth -= damage;
     }
 
-    void EnemyDeath()
+     IEnumerator EnemyDeath()
     {
+        if (isdead)
+        {
+            yield return new WaitForSeconds(0.5f);
             Destroy(gameObject);
+        }
     }
+
+    IEnumerator Burning()
+     {
+         yield return new WaitForSeconds(1);
+         GetComponent<EnemyBehaviour>().TakeDamage(BurningDamage);
+         
+
+
+     }
 
 }
